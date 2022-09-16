50cfa646f9c9e07c004a046bbfd4f579d1cb5c76
kaylee182000 <trungkien40414@gmail.com>
2022-09-17 00:11:15 +0700

    nav,footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="footer">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-4 col-sm-6">
+                    <h4>Get Help</h4>
+                    <div>
+                        <ul>
+                            <li>Home</li>
+                            <li>Nike</li>
+                            <li>Adidas</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-sm-6">
+                    <h4>Support</h4>
+                    <div>
+                        <ul>
+                            <li>About</li>
+                            <li>Contact</li>
+                            <li>Help</li>
+                            <li>Phone</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-sm-6">
+                    <h4>Register</h4>
+                    <div>
+                        <ul>
+                            <li>Register</li>
+                            <li>Login</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
